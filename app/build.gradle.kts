ffd142730b4bb20cba3131af8fd615f43a5d704d
Viktor Shchelochkov <hi@hloth.dev>
2026-08-28 00:47:09 +0200

    Bump the version code and add its changelog

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,7 +23,7 @@ android {
         applicationId = "dev.hloth.zaragoza_tarjeta_bus"
         minSdk = 26
         targetSdk = 36
-        versionCode = 1
+        versionCode = 2
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
