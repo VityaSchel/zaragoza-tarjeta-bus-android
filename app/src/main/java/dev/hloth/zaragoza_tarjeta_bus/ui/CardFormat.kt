701150ab6628eb833e310b2256ba5c6276296c6b
Viktor Shchelochkov <hi@hloth.dev>
2026-08-31 02:50:29 +0200

    Force "€" instead of "EUR"

--- app/src/main/java/dev/hloth/zaragoza_tarjeta_bus/ui/CardFormat.kt
+++ app/src/main/java/dev/hloth/zaragoza_tarjeta_bus/ui/CardFormat.kt
@@ -5,12 +5,14 @@ import androidx.compose.ui.res.stringResource
 import dev.hloth.zgztransport.Balance
 import dev.hloth.zgztransport.CardDate
 import dev.hloth.zgztransport.CardDateTime
+import java.text.DecimalFormat
 import java.text.NumberFormat
 import java.time.format.DateTimeFormatter
 import java.time.format.DecimalStyle
 import java.util.Locale
 import java.util.Currency
 
+private const val EURO_SIGN = "€"
 private const val DATE_PATTERN = "d MMM yyyy"
 private const val TIME_PATTERN = "HH:mm"
 
@@ -18,7 +20,12 @@ private const val TIME_PATTERN = "HH:mm"
 internal fun Label.text(): String = stringResource(id, *args.toTypedArray())
 
 internal fun Balance.formatted(): String = NumberFormat.getCurrencyInstance()
-    .apply { currency = Currency.getInstance("EUR") }
+    .apply {
+        currency = Currency.getInstance("EUR")
+        (this as? DecimalFormat)?.let {
+            it.decimalFormatSymbols = it.decimalFormatSymbols.apply { currencySymbol = EURO_SIGN }
+        }
+    }
     .format(euros())
 
 private fun formatter(pattern: String): DateTimeFormatter {
